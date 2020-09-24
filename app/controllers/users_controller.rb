10675b57a50ce2a3e7c802e0f64db493146f0c0e
Atie <ghaziatie@gmail.com>
2020-09-24 19:19:07 +0200

    testok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,27 +49,26 @@ class UsersController < ApplicationController
     flash[:success] = "Utilisateur supprimé."
     redirect_to users_path
   end
+end
 
 
   private
 
-    def edit
-      @user = User.find(params[:id])
-      @titre = "Édition profil"
-    end
+  def user_params
+    params.require(:user).permit(:nom, :email, :password, :password_confirmation, :salt, :encrypted_password)
+  end
 
-    def authenticate
-      deny_access unless signed_in?
-    end
+  def authenticate
+    deny_access unless signed_in?
+  end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 
-    def admin_user
-      redirect_to(root_path) unless current_user.admin?
-    end
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
 end
 
